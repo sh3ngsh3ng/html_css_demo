1a58240cf3a5b1a308cae16ddffa113e7a998526
sh3ngsh3ng <yshenglim13@gmail.com>
2022-03-22 17:03:36 +0000

    added fade-in css animation

--- script/animation.js
+++ script/animation.js
@@ -5,13 +5,31 @@ const observer = new IntersectionObserver(entries => {
 
         if (entry.isIntersecting) {
             sectionHeader.classList.add("animate-pop-in")
+            contentContainer.classList.add("slide-in")
             return
         }
+
 
         sectionHeader.classList.remove("animate-pop-in")
     })
 })
 
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+
+        const contentContainer = entry.target.querySelector(".test")
+
+        console.log(contentContainer)
+
+        if (entry.isIntersecting) {
+            contentContainer.classList.add("fade-in")
+            return
+        }
+
+
+        // sectionHeader.classList.remove("animate-pop-in")
+    })
+})
 
 let target = ".section-header-div"
 document.querySelectorAll(target).forEach((i) => {
@@ -20,3 +38,9 @@ document.querySelectorAll(target).forEach((i) => {
     }
 })
 
+let target2 = ".content-container"
+document.querySelectorAll(target2).forEach((i) => {
+    if (i) {
+        observer2.observe(i)
+    }
+})
