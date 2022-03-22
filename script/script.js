ac82a1b22c3d27245e678fdc5eff386f5b424a7e
sh3ngsh3ng <yshenglim13@gmail.com>
2022-03-22 18:59:43 +0000

    added datagov map instead of leaflet

--- script/script.js
+++ script/script.js
@@ -6,9 +6,11 @@
 document.querySelector("#start-btn").addEventListener("click", () => {
     document.querySelector("#main-page").classList.add("hide-page")
     document.querySelector("#map-page").classList.add("show-page")
+    document.querySelector("body").classList.add("clear-bottom")
 })
 
 document.querySelector("#back-btn").addEventListener("click", () => {
+    document.querySelector("body").classList.remove("clear-bottom")
     document.querySelector("#map-page").classList.remove("show-page")
     document.querySelector("#map-page").classList.add("hide-page")
     document.querySelector("#main-page").classList.remove("hide-page")
